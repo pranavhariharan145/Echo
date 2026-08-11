cdd4977a17ed4b324c6eff4fd209c76a29b20666
Pranav <pranavhariharan145@gmail.com>
2026-08-11 15:31:32 +0530

    Installed Dependencies and Setting up of Tavily Search

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from echo!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+import pandas as pd
 
+# Load environment Variables
+load_dotenv()
+deep_api_key = os.getenv("DEEPSEEK_API_KEY")
+tavily_api_key = os.getenv("TAVILY_API_KEY")
 
-if __name__ == "__main__":
-    main()
+# Setup Tavily Search
+
+search = TavilySearchResults(max_results = 1)
+
+result = search.invoke("Capital of France")
+print(result)
