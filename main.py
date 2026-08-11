4070366c3abe096703dc0ee8438f7be22fd36052
Pranav <pranavhariharan145@gmail.com>
2026-08-11 22:18:06 +0530

    Added Weather and Search as tool works fine

--- main.py
+++ main.py
@@ -8,39 +8,55 @@ from langchain_groq import ChatGroq
 import langchainhub as hub
 import pandas as pd
 from groq import Groq
+from langchain_core.tools import tool
+import requests, json
 
 # Load environment Variables
 load_dotenv()
 groq_api_key = os.getenv("GROQ_API_KEY")
 tavily_api_key = os.getenv("TAVILY_API_KEY")
+weather_stack_api = os.getenv("WEATHER_STACK_API")
 
 # Setup Tavily Search
 search = TavilySearchResults(max_results = 5)
-result = search.invoke("Chennai Weather")
+
+@tool
+def fetch_weather(city : str) -> str:
+    """Fetch Current Weather of the city"""
+    url = (f"http://api.weatherstack.com/current?access_key={weather_stack_api}&query={city}")
+    response = requests.get(url)
+    data = response.json()
+    current = data.get("current", {})
+    location = data.get("location", {})
+    weather_data = {
+        "Name": location.get("name"),
+        "Country": location.get("country"),
+        "Temperature": current.get("temperature"),
+        "Description": current.get("weather_descriptions"),
+        "Wind_Speed": current.get("wind_speed"),
+        "Precipitation": current.get("precip"),
+        "Humidity" : current.get("humidity"),
+        "feelslike": current.get("feelslike"),
+        "uv_index": current.get("uv_index")
+    }
+    return json.dumps(weather_data)
 
 # LLM Setup
 llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
-    temperature=0,
+    model="llama-3.1-8b-instant",
+    temperature=1,
 )
 
 # Setup Tool
-tools = [search]
+tools = [fetch_weather, search]
 
-# Setup Agent
-
-SYSTEM_PROMPT = """You are an Autonomous ReAct AI Research Assistant. You operate strictly using a continuous loop of Reasoning, Action, Observation, and Synthesis.
-
-### EXECUTION STEPS:
-1. **THINK**: Analyze the user input. Identify missing information, key entities, and required tool calls.
-2. **ACT**: Execute search tools with optimized keywords. Search one atomic concept at a time.
-3. **OBSERVE**: Analyze search tool outputs. Check if the retrieved data directly answers the query.
-4. **SYNTHESIZE**: Synthesize the observations into an structured, well-organized response.
-
-### CRITICAL CONSTRAINTS:
-- Do NOT answer questions requiring external facts without calling the search tool first.
-- Maintain an objective, professional, and analytical tone.
-- Format complex comparative data using Markdown tables or itemized bullet points.
+# Setup Agent with Appropriate Prompt
+SYSTEM_PROMPT = """
+When answering questions using tools results:
+- Use all relevant information returned by the tools.
+- Do not ignore useful fields.
+- For detailed answers, explain each relevant field.
+- For brief answers, summarize only the most important fields.
 """
 agent = create_agent(
     model=llm,
@@ -58,3 +74,9 @@ response = agent.invoke({
 })
 print("\n" + "="*50 + "\n")
 print(response["messages"][-1].content)
+
+
+# Custom Tools creation using functions also works
+
+
+
