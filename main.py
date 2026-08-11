c061e381024afd73c46fd11a6a1f39a6307dcdf5
Pranav <pranavhariharan145@gmail.com>
2026-08-11 17:54:35 +0530

    Done with Implementing the Agent

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+from langchain_core.prompts import PromptTemplate
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain.agents import create_agent
 from langchain_groq import ChatGroq
+import langchainhub as hub
 import pandas as pd
 from groq import Groq
 
@@ -14,13 +17,44 @@ tavily_api_key = os.getenv("TAVILY_API_KEY")
 # Setup Tavily Search
 search = TavilySearchResults(max_results = 5)
 result = search.invoke("Chennai Weather")
-df = pd.DataFrame(result)
-print(df)
 
 # LLM Setup
 llm = ChatGroq(
     model="llama-3.3-70b-versatile",
     temperature=0,
 )
-result = llm.invoke("Joke about AI?")
-print(result.content)
+
+# Setup Tool
+tools = [search]
+
+# Setup Agent
+
+SYSTEM_PROMPT = """You are an Autonomous ReAct AI Research Assistant. You operate strictly using a continuous loop of Reasoning, Action, Observation, and Synthesis.
+
+### EXECUTION STEPS:
+1. **THINK**: Analyze the user input. Identify missing information, key entities, and required tool calls.
+2. **ACT**: Execute search tools with optimized keywords. Search one atomic concept at a time.
+3. **OBSERVE**: Analyze search tool outputs. Check if the retrieved data directly answers the query.
+4. **SYNTHESIZE**: Synthesize the observations into an structured, well-organized response.
+
+### CRITICAL CONSTRAINTS:
+- Do NOT answer questions requiring external facts without calling the search tool first.
+- Maintain an objective, professional, and analytical tone.
+- Format complex comparative data using Markdown tables or itemized bullet points.
+"""
+agent = create_agent(
+    model=llm,
+    tools=tools,
+    system_prompt=SYSTEM_PROMPT,
+)
+print("\n" + "="*50 + "\n")
+userinput = input("Enter Your Query: ")
+
+# Setup executer
+response = agent.invoke({
+    "messages": [
+        {"role": "user", "content": userinput}
+    ]
+})
+print("\n" + "="*50 + "\n")
+print(response["messages"][-1].content)
