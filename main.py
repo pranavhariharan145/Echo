b06647589bbf1f2935abd274610440c33c8f368a
Pranav <pranavhariharan145@gmail.com>
2026-08-11 16:21:13 +0530

    LLM done

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_groq import ChatGroq
 import pandas as pd
+from groq import Groq
 
 # Load environment Variables
 load_dotenv()
-deep_api_key = os.getenv("DEEPSEEK_API_KEY")
+groq_api_key = os.getenv("GROQ_API_KEY")
 tavily_api_key = os.getenv("TAVILY_API_KEY")
 
 # Setup Tavily Search
+search = TavilySearchResults(max_results = 5)
+result = search.invoke("Chennai Weather")
+df = pd.DataFrame(result)
+print(df)
 
-search = TavilySearchResults(max_results = 1)
-
-result = search.invoke("Capital of France")
-print(result)
+# LLM Setup
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0,
+)
+result = llm.invoke("Joke about AI?")
+print(result.content)
